cf0d459b03efb3b7142bf62b7bc17bcd3c9851e0
Owen175 <oclocliphone@icloud.com>
2023-09-22 20:00:10 +0100

    Set up board

--- chess.py
+++ chess.py
@@ -1,6 +1,31 @@
 from pieces import King, Queen, Pawn, Bishop, Rook, Knight, Side
 
-black = Side(King(1,1), Queen(1,1), Bishop(1,1), Bishop(1,1), Knight(1,1), Knight(1,1), Rook(1,1), Rook(1,1), Pawn(1,1), Pawn(1,1), Pawn(1,1), Pawn(1,1), Pawn(1,1), Pawn(1,1), Pawn(1,1), Pawn(1,1))
-white = Side(King(1,1), Queen(1,1), Bishop(1,1), Bishop(1,1), Knight(1,1), Knight(1,1), Rook(1,1), Rook(1,1), Pawn(1,1), Pawn(1,1), Pawn(1,1), Pawn(1,1), Pawn(1,1), Pawn(1,1), Pawn(1,1), Pawn(1,1))
 
-print(black.king.x)
+class Board:
+    def __init__(self, blk, wht):
+        self.blk = blk
+        self.wht = wht
+        # Both of these update with the black and white sides as they are.
+        self.board = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+    def setup_board(self):
+        for blk_piece in self.blk:
+            x, y = blk_piece.x, blk_piece.y
+            if self.board[x][y] != 0:
+                print('2 pieces have been given the same start position. Error.')
+            self.board[blk_piece.x][blk_piece.y] = blk_piece
+
+
+black = Side(King(5, 7, False), Queen(4, 7, False), Bishop(2, 7, False), Bishop(5, 7, False), Knight(1, 7, False),
+             Knight(6, 7, False), Rook(0, 7, False), Rook(7, 7, False), Pawn(0, 6, False), Pawn(1, 6, False),
+             Pawn(2, 6, False), Pawn(3, 6, False), Pawn(4, 6, False), Pawn(5, 6, False), Pawn(6, 6, False),
+             Pawn(7, 6, False))
+
+white = Side(King(5, 0, True), Queen(4, 0, True), Bishop(2, 0, True), Bishop(5, 0, True), Knight(1, 0, True),
+             Knight(6, 0, True), Rook(0, 0, True), Rook(7, 0, True), Pawn(0, 1, True), Pawn(1, 1, True),
+             Pawn(2, 1, True), Pawn(3, 1, True), Pawn(4, 1, True), Pawn(5, 1, True), Pawn(6, 1, True), Pawn(7, 1, True))
+
+chessboard = Board(black, white)
+black.king.x = 1
+chessboard.test()
--- pieces.py
+++ pieces.py
@@ -1,39 +1,59 @@
 class Side:
-    def __init__(self, king, queen, bishop1, bishop2, knight1, knight2, rook1, rook2, pawn1, pawn2, pawn3, pawn4, pawn5, pawn6, pawn7, pawn8):
+    def __init__(self, king, queen, bishop1, bishop2, knight1, knight2, rook1, rook2, pawn1, pawn2, pawn3, pawn4, pawn5,
+                 pawn6, pawn7, pawn8):
         self.king = king
         self.queen = queen
         self.bishop1 = bishop1
         self.bishop2 = bishop2
         self.knight1, self.knight2 = knight1, knight2
         self.rook1, self.rook2 = rook1, rook2
-        self.pawn1, self.pawn2, self.pawn3, self.pawn4, self.pawn5, self.pawn6, self.pawn7, self.pawn8 = pawn1, pawn2, pawn3, pawn4, pawn5, pawn6, pawn7, pawn8
+        self.pawn1, self.pawn2, self.pawn3, self.pawn4, self.pawn5, self.pawn6, self.pawn7, self.pawn8 = pawn1, pawn2,\
+            pawn3, pawn4, pawn5, pawn6, pawn7, pawn8
+        self.piece_list = [self.king, self.queen, self.bishop1, self.bishop2, self.knight1, self.knight2, self.rook1,
+                           self.rook2, self.pawn1, self.pawn2, self.pawn3, self.pawn4, self.pawn5, self.pawn6,
+                           self.pawn7, self.pawn8]
 
 class Pawn:
-    def __init__(self, x, y):
+    def __init__(self, x, y, colour):
         self.x = x
         self.y = y
+        self.colour = colour
+        self.hasMoved = False
+
+    def movable_spots(self):
+        if self.colour:
+            x_change = 1
+        else:
+            x_chance = -1
+
+
 
 class Bishop:
-    def __init__(self, x, y):
+    def __init__(self, x, y, colour):
         self.x = x
         self.y = y
+        self.colour = colour
 
 class Queen:
-    def __init__(self, x, y):
+    def __init__(self, x, y, colour):
         self.x = x
         self.y = y
+        self.colour = colour
 
 class King:
-    def __init__(self, x, y):
+    def __init__(self, x, y, colour):
         self.x = x
         self.y = y
+        self.colour = colour
 
 class Rook:
-    def __init__(self, x, y):
+    def __init__(self, x, y, colour):
         self.x = x
         self.y = y
+        self.colour = colour
 
 class Knight:
-    def __init__(self, x, y):
+    def __init__(self, x, y, colour):
         self.x = x
         self.y = y
+        self.colour = colour
